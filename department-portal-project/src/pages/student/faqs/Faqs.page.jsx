1624a2cfa75290a90cc31574ff31c208c788536b
Tobias <redefineddevelopertee@gmail.com>
2023-08-20 22:59:42 +0100

    first commit

---
+++ department-portal-project/src/pages/student/faqs/Faqs.page.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Faqs = () => {
+  return (
+    <div>
+      I WANT TO BE PART OF NACOS EXCOs,HOW DO I BE PART OF IT?
+Here are the criteria’s to be an Exco:
+*Must be a Nacossite
+*Must be up to date on Nacoss dues receipt
+*Must have an affidavit that shows he/she is free from any secret cult
+*Must not be occupying any official position in the department( if reverse is the case, the
+person is meant to resign 24hrs before screening and resignation letter must be brought to
+the board on the day of the screening)
+* Must have a CGPA of 3.5 for president and vice president while 3.0 for other post.
+First years are excluded as they don't have a CGPA currently
+*Must be free from any security scandals
+Positions and form prices:
+PRESIDENCY:
+PRESIDENT must be in 300level going to final year
+VICE PRESIDENT must be in 200level going to 300level
+The following posts are available for third years and second years
+SECRETARY GENERAL:
+TREASURER:
+DIRECTOR OF SOCIALS:
+WELFARE1: 
+DIRECTOR OF SOFTWARE:
+DIRECTOR OF GAMES:
+FINANCIAL SECRETARY:
+The following posts are open for second and first years only.
+ASSISTANT SECRETARY GENERAL:
+DIRECTOR OF SOCIALS II :
+WELFARE II :
+DIRECTOR OF SOFTWARE II:
+LIBARIAN:
+These positions is available for the first years only.
+PROVOST:
+PUBLIC RELATIONS OFFICER:
+Get your forms from Nacos office at the required time
+    </div>
+  )
+}
+
+export default Faqs
